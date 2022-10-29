77ebf9d04c76678cb0d5a009f451260ba859c3e3
mudit999 <muditgarg1211@gmail.com>
2022-10-29 12:02:50 +0530

    initial version

---
+++ controllers/userControllers.js
@@ -0,0 +1,52 @@
+const User = require("../models/userModel");
+
+// Register
+const registerUser = async (req,res) => {
+    const { name, email, password } = req.body;
+    
+    // checking if user already exists
+    const userExists = await User.findOne({ email });
+
+    if(userExists){
+        res.status(400);
+        throw new Error("User already Exists");
+    }
+
+    // adding entry in collection
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    }else{
+        res.status(400);
+        throw new Error("Error Occured ")
+    }
+};
+
+// Login
+const authUser = async (req,res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    }else{
+        res.status(400);
+        throw new Error("Invalid Email or Password")
+    }
+};
+
+module.exports = {registerUser, authUser};
---
+++ server.js
@@ -0,0 +1,24 @@
+// Web server
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
+
+const app = express();
+dotenv.config();
+connectDB();
+
+// For accepting json data from user
+app.use(express.json());
+
+app.use('/api/users',userRoutes);
+app.use('/api/notes', noteRoutes);
+
+app.get("/",(req, res) => {
+    res.send("Backend is running...");
+});
+
+const PORT = process.env.PORT || 5320;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
